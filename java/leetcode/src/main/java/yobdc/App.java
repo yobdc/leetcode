965abd1bdea2d0a3c3cd44fc73e27bdb5d5a2dce
Lex <yobdc.101@gmail.com>
2018-02-22 13:40:29 +0800

    第1题

--- java/leetcode/src/main/java/yobdc/App.java
+++ java/leetcode/src/main/java/yobdc/App.java
@@ -5,6 +5,6 @@ package yobdc;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println(new Problem50().myPow(2.0000, 2147483647));
+        System.out.println(new Problem1().twoSum(new int[]{1,2, 7, 11, 15}, 9));
     }
 }
