e1df4c252a8cadb70096ea2bdf79eeba40825340
Lex <yobdc.101@gmail.com>
2018-02-23 14:29:38 +0800

    第3题

--- java/leetcode/src/main/java/yobdc/App.java
+++ java/leetcode/src/main/java/yobdc/App.java
@@ -5,6 +5,6 @@ package yobdc;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println(new Problem2().addTwoNumbers(new Problem2().makeL1(), new Problem2().makeL2()));
+        System.out.println(new Problem3().lengthOfLongestSubstring("abaab"));
     }
 }
---
+++ java/leetcode/src/main/java/yobdc/Problem3.java
@@ -0,0 +1,34 @@
+package yobdc;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem3 {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<Character>();
+        int start = 0;
+        int current = 0;
+        int maxSize = 0;
+        int strSize = s.length();
+
+        while (start + maxSize < strSize) {
+            char c = s.charAt(start + current);
+            while (!charSet.contains(c)) {
+                charSet.add(c);
+                current++;
+                if (start + current < strSize) {
+                    c = s.charAt(start + current);
+                } else {
+                    break;
+                }
+            }
+            maxSize = maxSize > current ? maxSize : current;
+            start = s.indexOf(c, start) + 1;
+            charSet.clear();
+            current = 0;
+        }
+
+        return maxSize;
+    }
+}
