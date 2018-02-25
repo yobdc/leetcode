95cad386e02bca37787a81d26d9ef836be6db83e
yobdc <yobdc.101@gmail.com>
2018-02-25 16:58:50 +0800

    第6题优化，不许关注列index，只需append

--- java/leetcode/src/main/java/yobdc/Problem6.java
+++ java/leetcode/src/main/java/yobdc/Problem6.java
@@ -6,44 +6,38 @@ public class Problem6 {
         if (numRows == 1) {
             return s;
         }
-        int numCols = s.length()/2+1;
-        char[][] mat = new char[numRows][numCols];
+
+        StringBuffer[] mat = new StringBuffer[numRows];
 
         int rowIndex = 0;
-        int colIndex = 0;
         int rowStep = 1;
-        int colStep = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            mat[rowIndex][colIndex] = s.charAt(i);
+            if (mat[rowIndex] == null) {
+                mat[rowIndex] = new StringBuffer();
+            }
+            mat[rowIndex].append(s.charAt(i));
             if (rowIndex + rowStep >= 0 && rowIndex + rowStep < numRows) {
                 rowIndex += rowStep;
             }
-            if (colIndex + colStep >= 0 && colIndex + colStep < numCols) {
-                colIndex += colStep;
-            }
             if (rowIndex == 0) {
                 rowStep = 1;
-                colStep = 0;
             }
             if (rowIndex == numRows - 1) {
                 rowStep = -1;
-                colStep = 1;
             }
         }
 
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (mat[i][j] != 0) {
-                    sb.append(mat[i][j]);
-                }
+            if (mat[i] != null && !mat[i].equals("")) {
+                sb.append(mat[i]);
             }
         }
         return sb.toString();
     }
 
     public void test() {
-        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("ABC", 2));
     }
 }
