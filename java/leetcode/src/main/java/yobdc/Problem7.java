a4b359b48e7e1bc4806114e1a98cf6067a7a55d1
Lex <yobdc.101@gmail.com>
2018-02-26 09:34:10 +0800

    第7题，注意int越界

--- java/leetcode/src/main/java/yobdc/Problem7.java
+++ java/leetcode/src/main/java/yobdc/Problem7.java
@@ -2,10 +2,30 @@ package yobdc;
 
 public class Problem7 {
     public int reverse(int x) {
-
+        if (x == 0) {
+            return x;
+        }
+        int current = x;
+        boolean negative = false;
+        if (x < 0) {
+            negative = true;
+            current = -x;
+        }
+        long result = 0;
+        int left = 0;
+        int base = 1;
+        while (current > 0) {
+            left = current % 10;
+            current = current / 10;
+            result = result * 10 + left;
+            if (result > Integer.MAX_VALUE) {
+                return 0;
+            }
+        }
+        return negative ? (int) -result : (int) result;
     }
 
     public void test() {
-        System.out.println(reverse(123));
+        System.out.println(reverse(1534236469));
     }
 }
