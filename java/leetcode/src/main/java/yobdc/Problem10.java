dde0d112c42f53b409235911b756df22616a69e4
Lex <yobdc.101@gmail.com>
2018-02-27 09:06:04 +0800

    第10题，准备

---
+++ java/leetcode/src/main/java/yobdc/Problem10.java
@@ -0,0 +1,18 @@
+package yobdc;
+
+public class Problem10 {
+    public boolean isMatch(String s, String p) {
+
+    }
+
+    public void test() {
+        System.out.println(isMatch("aaa","aa"));
+//        isMatch("aa","a") → false
+//        isMatch("aa","aa") → true
+//        isMatch("aaa","aa") → false
+//        isMatch("aa", "a*") → true
+//        isMatch("aa", ".*") → true
+//        isMatch("ab", ".*") → true
+//        isMatch("aab", "c*a*b") → true
+    }
+}
