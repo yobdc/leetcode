90c2e5ac818d3c74040a3c47f52b3cc43b0a0b41
Lex <yobdc.101@gmail.com>
2018-03-01 14:28:56 +0800

    not correct

--- java/leetcode/src/main/java/yobdc/Problem10.java
+++ java/leetcode/src/main/java/yobdc/Problem10.java
@@ -9,56 +9,80 @@ public class Problem10 {
             return true;
         }
         boolean match = true;
-        if (!p.contains("*")) {
-            if (p.length() != s.length()) {
-                return false;
-            } else {
-                for (int i = 0; i < p.length(); i++) {
-                    char pChar = p.charAt(i);
-                    if (pChar != '.' && pChar != s.charAt(i)) {
-                        return false;
+
+        int sIndex = 0;
+        int pIndex = 0;
+        char prePChar = 0;
+        char postPChar = 0;
+        while (sIndex < s.length() && pIndex < p.length()) {
+            char sChar = s.charAt(sIndex);
+            char pChar = p.charAt(pIndex);
+
+            if (p.length() - pIndex > s.length() - sIndex) {
+                if (pIndex + 1 < p.length()) {
+                    if (p.charAt(pIndex + 1) == '*') {
+                        pIndex += 2;
+                        continue;
                     }
                 }
             }
-        } else {
-            String[] pList = p.split("\\*");
-            int startIndex = 0;
-            for (int i = 0; i < pList.length; i++) {
-                String pStr = pList[i];
-                if (pStr == null || pStr.isEmpty()) {
-                    continue;
-                }
-                int tmpStart = indexOf(s, pStr, startIndex);
-                if (tmpStart < 0) {
-                    return false;
+
+            if (pChar == '.') {
+                sIndex++;
+                pIndex++;
+                prePChar = pChar;
+            } else if (pChar == '*') {
+                if (prePChar == '.' || prePChar == sChar) {
+                    sIndex++;
+                    if (p.length() - pIndex > s.length() - sIndex) {
+                        pIndex++;
+                    }
                 } else {
-                    startIndex += pStr.length();
+                    if (pIndex + 1 < p.length()) {
+                        postPChar = p.charAt(pIndex + 1);
+                    } else {
+                        postPChar = 0;
+                    }
+                    if (postPChar == sChar) {
+                        sIndex++;
+                        pIndex += 2;
+                        prePChar = postPChar;
+                    } else {
+                        return false;
+                    }
                 }
-            }
-            if (startIndex < s.length() && p.charAt(p.length() - 1) != '*') {
-                return false;
+            } else {
+                if (sChar != pChar) {
+                    if (pIndex + 1 < p.length()) {
+                        postPChar = p.charAt(pIndex + 1);
+                    }
+                    if (postPChar == '*') {
+                        pIndex++;
+                        prePChar = pChar;
+                        continue;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    sIndex++;
+                    pIndex++;
+                    prePChar = pChar;
+                }
             }
         }
+        if (sIndex - s.length() != pIndex - p.length()) {
+            if (pIndex + 1 == p.length() && p.charAt(pIndex) == '*') {
+                match = true;
+            } else {
+                match = false;
+            }
+        }
+
         return match;
     }
 
-    private int indexOf(String src, String tar, int startIndex) {
-        if (tar.contains(".")) {
-            boolean found = false;
-            for (int i = 0; i < src.length() - startIndex - tar.length(); i++) {
-                if (src.charAt(startIndex + i) == tar.charAt(i) || tar.charAt(i) == '.') {
-                    continue;
-                } else {
-
-                }
-            }
-        } else {
-            return src.indexOf(tar, startIndex);
-        }
-    }
-
     public void test() {
-        System.out.println(isMatch("abbbc", "a*b*"));
+        System.out.println(isMatch("aaa", "a*a*a"));
 //        isMatch("aa","a") → false
 //        isMatch("aa","aa") → true
 //        isMatch("aaa","aa") → false
