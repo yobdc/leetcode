72bec0e60e74e147071fc8149a7b73af4e63da00
Lex <yobdc.101@gmail.com>
2018-03-01 17:06:13 +0800

    第11题准备

--- java/leetcode/src/main/java/yobdc/App.java
+++ java/leetcode/src/main/java/yobdc/App.java
@@ -5,6 +5,6 @@ package yobdc;
  */
 public class App {
     public static void main(String[] args) {
-        new Problem10().test();
+        new Problem11().test();
     }
 }
---
+++ java/leetcode/src/main/java/yobdc/Problem11.java
@@ -0,0 +1,29 @@
+package yobdc;
+
+import java.util.Date;
+
+public class Problem11 {
+    public int maxArea(int[] height) {
+        Date start = new Date();
+        int maxArea = 0;
+
+        for (int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int tmpArea = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
+                maxArea = maxArea > tmpArea ? maxArea : tmpArea;
+            }
+        }
+
+        Date end = new Date();
+        System.out.println(end.getTime() - start.getTime());
+        return maxArea;
+    }
+
+    public void test() {
+        int[] arr = new int[15000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr.length - i;
+        }
+        System.out.println(maxArea(arr));
+    }
+}
