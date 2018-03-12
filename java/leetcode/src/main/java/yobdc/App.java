c2f13c7a0b2fc58eff4af309f45ac0a241d3b532
Lex <yobdc.101@gmail.com>
2018-03-12 16:42:54 +0800

    prepare

--- java/leetcode/src/main/java/yobdc/App.java
+++ java/leetcode/src/main/java/yobdc/App.java
@@ -5,6 +5,6 @@ package yobdc;
  */
 public class App {
     public static void main(String[] args) {
-        new Problem13().test();
+        new Problem14().test();
     }
 }
---
+++ java/leetcode/src/main/java/yobdc/Problem14.java
@@ -0,0 +1,12 @@
+package yobdc;
+
+public class Problem14 {
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+
+    public void test() {
+        String[] strs = {"aaa", "aab"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
