38f3b4cde50edc9c716d26070291bf79afd07f33
Lex <yobdc.101@gmail.com>
2018-03-13 22:27:27 +0800

    fix typo

--- java/leetcode/src/main/java/yobdc/Problem14.java
+++ java/leetcode/src/main/java/yobdc/Problem14.java
@@ -2,7 +2,24 @@ package yobdc;
 
 public class Problem14 {
     public String longestCommonPrefix(String[] strs) {
-
+		if(strs.length()==0){
+			return null;
+		}
+		if(strs.length()==1){
+			return strs[0];
+		}
+		String tar = strs[0];
+		StringBuffer sb = new StringBuffer();
+		int index = 0;
+		while(index<tar.length()){
+			for(int i=1;i<strs.length();i++){
+				if(tar.charAt(index)!=str[i].charAt(index)){
+					return sb.toString()
+				}
+			}
+			sb.append(tar.charAt(index));
+		}
+		return sb.toString();
     }
 
     public void test() {
