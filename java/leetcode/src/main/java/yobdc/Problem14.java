f9e7cbdeb3767ee15e63245f5ad4878cf436d684
Lex <yobdc.101@gmail.com>
2018-03-21 13:33:54 +0800

    第14题

--- java/leetcode/src/main/java/yobdc/Problem14.java
+++ java/leetcode/src/main/java/yobdc/Problem14.java
@@ -2,28 +2,28 @@ package yobdc;
 
 public class Problem14 {
     public String longestCommonPrefix(String[] strs) {
-		if(strs.length()==0){
-			return null;
-		}
-		if(strs.length()==1){
-			return strs[0];
-		}
-		String tar = strs[0];
-		StringBuffer sb = new StringBuffer();
-		int index = 0;
-		while(index<tar.length()){
-			for(int i=1;i<strs.length();i++){
-				if(tar.charAt(index)!=str[i].charAt(index)){
-					return sb.toString()
-				}
-			}
-			sb.append(tar.charAt(index));
-		}
-		return sb.toString();
+        if (strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        String tar = strs[0];
+        StringBuffer sb = new StringBuffer();
+        int index = 0;
+        while (index < tar.length()) {
+            for (int i = 1; i < strs.length; i++) {
+                if (index >= strs[i].length() || tar.charAt(index) != strs[i].charAt(index)) {
+                    return sb.toString();
+                }
+            }
+            sb.append(tar.charAt(index++));
+        }
+        return sb.toString();
     }
 
     public void test() {
-        String[] strs = {"aaa", "aab"};
+        String[] strs = {"aaa", "a"};
         System.out.println(longestCommonPrefix(strs));
     }
 }
